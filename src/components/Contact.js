7f153f18aa4b7bae975e2a148b60a64c9b247922
Lucie Sundermeijer <lucie.sun@gmail.com>
2023-01-11 22:49:28 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,15 @@ import React from "react"
 
 function Contact() {
 	return (
-		<div>Contact</div>
+		<section id="contact" className="section px-[30px] pb-[80px]">
+			<div className="max-w-[1140px] mx-auto w-full">
+				<form className="flex flex-col gap-y-10">
+					<input className="border-b border-dark placeholder:text-[#555] italic tracking-wider pb-2" placeholder="Votre nom" type="text" />
+					<input className="border-b border-dark placeholder:text-[#555] italic tracking-wider pb-2" placeholder="Votre adresse e-mail" type="email" />
+					<input className="border-b border-dark placeholder:text-[#555] italic tracking-wider pb-2" placeholder="Votre message" type="text" />
+				</form>
+			</div>
+		</section>
 	)
 }
 
