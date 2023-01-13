1a2e22414c3f83b6f4ecaa3470ba55de5f1c0911
Lucie Sundermeijer <lucie.sun@gmail.com>
2023-01-13 20:09:14 +0100

    Add animations on Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,56 @@
 import React from "react"
+//import icons
 import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from "react-icons/fa"
+// import motion
+import { motion } from 'framer-motion';
+
+const textLeft = {
+	hidden: {
+		x: -100,
+		opacity: 0,
+	},
+	show: {
+		x: 0,
+		opacity: 1,
+		transition: {
+			type: 'tween',
+			duration: 1.4,
+		},
+	},
+}
+
+const textRight = {
+	hidden: {
+		x: 100,
+		opacity: 0,
+	},
+	show: {
+		x: 0,
+		opacity: 1,
+		transition: {
+			type: 'tween',
+			duration: 1.4,
+		},
+	},
+}
 
 function Contact() {
 	return (
 		<section id="contact" className="section px-[30px] pb-[80px]">
 			<div className="max-w-[1140px] mx-auto w-full">
-				<h2>Contact :</h2>
-				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+				<motion.div 
+					variants={textLeft}
+					initial="hidden"
+					whileInView={"show"}>
+					<h2>Contact :</h2>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+				</motion.div>
 				<div className="flex flex-col lg:flex-row lg:justify-between">
-					<div className="flex flex-col gap-y-4 mt-14 py-4 w-full">
+					<motion.div 
+						variants={textLeft}
+						initial="hidden"
+						whileInView={"show"}
+						className="flex flex-col gap-y-4 mt-14 py-4 w-full">
 						<div className="flex items-center gap-x-4">
 							<span><FaMapMarkerAlt /></span>
 							<span>633 Hayes Crest, Los Angeles</span>
@@ -21,19 +63,22 @@ function Contact() {
 							<span><FaEnvelope /></span>
 							<span>contact@contact.com</span>
 						</div>
-					</div>
-					<div className="mt-14 w-full">
+					</motion.div>
+					<motion.div 
+						variants={textRight}
+						initial="hidden"
+						whileInView={"show"}
+						className="mt-14 w-full">
 						<form className="flex flex-col gap-y-10">
 							<input className="border-b border-dark placeholder:text-[#555] italic tracking-wider pb-2" placeholder="Votre nom" type="text" />
 							<input className="border-b border-dark placeholder:text-[#555] italic tracking-wider pb-2" placeholder="Votre adresse e-mail" type="email" />
 							<input className="border-b border-dark placeholder:text-[#555] italic tracking-wider pb-2" placeholder="Votre message" type="text" />
 							<button className="btn bg-zinc-900 text-white hover:bg-zinc-900/90">Envoyer</button>
 						</form>
-					</div>
+					</motion.div>
 				</div>
 			</div>
 		</section>
 	)
 }
-
 export default Contact
